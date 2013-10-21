c7a12e723740ecb5f29f88a20f5a9fb987d4c6e9
Aleksandar Milicevic <aleks@csail.mit.edu>
2013-10-21 12:56:53 -0400

    added a filtering policy

--- app/models/security_model.rb
+++ app/models/security_model.rb
@@ -15,32 +15,12 @@ Red::Dsl.security_model do
 
     read User.status.when do |user|
       client.user == user ||
-        !server.rooms.none? {|room| ([user, client.user] - room.members).empty?}
+        server.rooms.some? {|room| ([user, client.user] - room.members).empty?}
+    end
+
+    restrict ChatRoom.messages.reject do |room, msg|
+      msg.sender != client.user &&
+        (msg.text.starts_with?("@") && !msg.text.starts_with?("@#{client.user.name} "))
     end
   end
-
-  # policy HideUserPrivateData do
-  #   principal client: Client
-
-  #   restrict User.password_hash.unless do |user, pswd|
-  #     client.user == user
-  #   end
-
-  #   restrict User.status.when do |user, status|
-  #     client.user != user &&
-  #     ChatRoom.none? { |room|
-  #       room.members.include?(client.user) &&
-  #       room.members.include?(user)
-  #     }
-  #   end
-  # end
-
-  # policy FilterChatRoomMembers do
-  #   principal client: Client
-
-  #   restrict ChatRoom.members.reject do |room, member|
-  #     !room.messages.sender.include?(member) &&
-  #     client.user != member
-  #   end
-  # end
 end
